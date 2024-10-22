194a494c2d08de904ad06d054e593842c6db38f1
Bruno Martins <brunodefatimoms@gamil.com>
2024-10-22 20:45:42 -0300

    Codigo para mostrar um formato de tabela

--- src/Consultorio/Medico.java
+++ src/Consultorio/Medico.java
@@ -54,8 +54,11 @@ public class Medico {
 	    
 	    Medico[] cabecalho = {"idMedico", "Nome", "CRM", "Especialidade", "Telefone", "Periodo de Atendimento"};
 	    listaMedico.add(cabecalho);
-	    for(int i = 0; i < listaMedicos.size(); i++){
+	    /*for(int i = 0; i < listaMedicos.size(); i++){
 	      System.out.println(listaMedicos.get(i).nomeMedico + "\t\t" + listaMedicos.get(i).crm + "\t" + listaMedicos.get(i).especialidade);
+	    */
+	    for(Medico[] novoMedico : listaMedico) {
+	    	System.out.printf("%-15s %10s %15s %n", listaMedico[0], listaMedico[1], listaMedico[2]);
 	    }
 	    
 	    System.out.println("\n\n\n");
